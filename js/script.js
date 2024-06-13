daf29000f824fbb392f3d3a17fdf83a1d7e7db17
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-06-13 02:46:01 +0000

    add first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Get API info.
+ */
+
+async function showCountry() {
+  try {
+    const resultJSON = await fetch("https://apiv3.iucnredlist.org/api/v3/country/list?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee")
+    const jsonData = await resultJSON.json()
+    const numberOfCountry = jsonData["count"]
+    const randomNumber = Math.floor(Math.random() * numberOfCountry) + 1  // returns a random integer from 1 to 6 into variable "randomNumber" 
+    const randomCountry = jsonData["results"][randomNumber]
+    console.log(randomCountry)
+    document.getElementById("result").innerHTML = "The random country is " + randomCountry
+  } catch (error) {
+    console.error(error)
+  }
+}
